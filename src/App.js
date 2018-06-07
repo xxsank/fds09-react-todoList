c78c3bdc1e4cf50ae6bd91a9c96de617bb43e861
Kim Sanghyeok <skyee23@gmail.com>
2018-06-07 15:38:31 +0900

    todos를 state로 이전

--- src/App.js
+++ src/App.js
@@ -10,18 +10,36 @@ const todos = [
     id:2,
     body: 'Redux 공부',
     complete: false
-  },
+  }
 ];
 
+
+
 class App extends Component {
+  state = {
+    todos:[
+      {
+        id:1,
+        body: 'React 공부',
+        complete: true
+      },
+      {
+        id:2,
+        body: 'Redux 공부',
+        complete: false
+      }
+    ]
+  }
+
   render() {
+    const {todos} = this.state;
     return (
      <div>
        <h1>할 일 목록</h1>
        <ul>
          {
            todos.map(todo => (
-             <li key={todo.id}>{todo.body}</li>
+             <li className={todo.complete ? 'complete' : ''} key={todo.id}>{todo.body}</li>
            ))
          }
        </ul>
