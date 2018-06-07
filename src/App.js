ea3e040e268403ccc3b198ff8ed618132ad544de
Kim Sanghyeok <skyee23@gmail.com>
2018-06-07 15:57:11 +0900

    할 일 추가 기능

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
 import React, { Component } from 'react';
 
+let count = 0;
+
 const todos = [
   {
-    id:1,
+    id:count++,
     body: 'React 공부',
     complete: true
   },
   {
-    id:2,
+    id:count++,
     body: 'Redux 공부',
     complete: false
   }
 ];
 
 
-
 class App extends Component {
   state = {
     todos:[
       {
-        id:1,
+        id:count++,
         body: 'React 공부',
         complete: true
       },
       {
-        id:2,
+        id:count++,
         body: 'Redux 공부',
         complete: false
       }
-    ]
+    ],
+    newTodoBody: ''
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      newTodoBody: e.target.value
+    });
+  }
+
+  handleButtonClick = e => {
+    if(this.state.newTodoBody){
+      const newTodo = {
+        body: this.state.newTodoBody,
+        complete: false,
+        id: count++
+      }
+
+      this.setState({
+        todos: [
+          ...this.state.todos,
+          newTodo
+        ],
+        newTodoBody: ''
+      })
+    }
   }
 
   render() {
-    const {todos} = this.state;
+    const {todos, newTodoBody} = this.state;
     return (
      <div>
        <h1>할 일 목록</h1>
+        <label>
+          새 할일
+          <input type="text" value={newTodoBody} onChange={this.handleInputChange}/>
+          <button onClick={this.handleButtonClick}>추가</button>
+        </label>
        <ul>
          {
            todos.map(todo => (
