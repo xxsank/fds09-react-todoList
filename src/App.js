15b4f6f32e4ce727214e42be81917edaac6f34be
Kim Sanghyeok <skyee23@gmail.com>
2018-06-10 22:57:57 +0900

    code edit

--- src/App.js
+++ src/App.js
@@ -1,109 +1,116 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 let count = 0;
 
 class App extends Component {
   state = {
-    todos:[
+    todos: [
       {
-        id:count++,
-        body: 'React 공부',
+        id: count++,
+        body: "React 공부",
         complete: true
       },
       {
-        id:count++,
-        body: 'Redux 공부',
+        id: count++,
+        body: "Redux 공부",
         complete: false
       }
     ],
-    newTodoBody: ''
-  }
+    newTodoBody: ""
+  };
 
   handleInputChange = e => {
     this.setState({
       newTodoBody: e.target.value
     });
-  }
+  };
 
   handleButtonClick = e => {
-    if(this.state.newTodoBody){
+    if (this.state.newTodoBody) {
       const newTodo = {
         body: this.state.newTodoBody,
         complete: false,
         id: count++
-      }
+      };
 
       this.setState({
-        todos: [
-          ...this.state.todos,
-          newTodo
-        ],
-        newTodoBody: ''
-      })
+        todos: [...this.state.todos, newTodo],
+        newTodoBody: ""
+      });
     }
-  }
+  };
 
   handleTodoItemComplete = id => {
-     this.setState({
-       todos: this.state.todos.map(t => {
-         const newTodo =  {
-           ...t
-         };
-         if(t.id === id){
-           newTodo.complete = true;
-         }
-         return newTodo;
-       })
+    this.setState({
+      todos: this.state.todos.map(t => {
+        const newTodo = {
+          ...t
+        };
+        if (t.id === id) {
+          newTodo.complete = true;
+        }
+        return newTodo;
       })
-  }
+    });
+  };
 
   handleTodoItemDelete = id => {
-      this.setState({
-        todos: this.state.todos.filter(t => id !== t.id)
-      })
-  }
+    this.setState({
+      todos: this.state.todos.filter(t => id !== t.id)
+    });
+  };
 
   render() {
-    const {todos, newTodoBody} = this.state;
+    const { todos, newTodoBody } = this.state;
     return (
-     <div>
-       <h1>할 일 목록</h1>
+      <div>
+        <h1>할 일 목록</h1>
         <label>
           새 할일
-          <input type="text" value={newTodoBody} onChange={this.handleInputChange}/>
+          <input
+            type="text"
+            value={newTodoBody}
+            onChange={this.handleInputChange}
+          />
           <button onClick={this.handleButtonClick}>추가</button>
         </label>
-       <ul>
-         {
-           todos.map(todo => 
-            <TodoItem 
-              key={todo.id} 
+        <ul>
+          {todos.map(todo => (
+            <TodoItem
+              key={todo.id}
               {...todo}
-              onComplete={this.handleTodoItemComplete} 
+              onComplete={this.handleTodoItemComplete}
               onDelete={this.handleTodoItemDelete}
             />
-          )
-         }
-       </ul>
-     </div>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
 
-class TodoItem extends Component{
+class TodoItem extends Component {
   render() {
-    const {id,body,complete, onComplete, onDelete} = this.props;
+    const { id, body, complete, onComplete, onDelete } = this.props;
     return (
-      <li className={complete ? 'complete' : ''} key={id}>
-       {body}
-       <button onClick={e => {
-        onComplete(id);
-       }}>완료</button>        
-       <button onClick={e => {
-        onDelete(id);
-       }}>삭제</button>
+      <li className={complete ? "complete" : ""} key={id}>
+        {body}
+        <button
+          onClick={e => {
+            onComplete(id);
+          }}
+        >
+          완료
+        </button>
+        <button
+          onClick={e => {
+            onDelete(id);
+          }}
+        >
+          삭제
+        </button>
       </li>
-    )
+    );
   }
 }
 
